235377708f966cae53d678b4b9ea3c80f0dfe151
Garret Raziel <boloomka@gmail.com>
2015-02-13 15:08:29 +0100

    beginning of test logging

---
+++ lib/anacondalog.pm
@@ -0,0 +1,22 @@
+package anacondalog;
+use base 'basetest';
+
+use testapi;
+
+sub post_fail_hook {
+    my $self = shift;
+    send_key "ctrl-alt-f2";
+    if (check_screen "anaconda_console", 10) {
+        upload_logs "/tmp/X.log";
+        upload_logs "/tmp/anaconda.log";
+        upload_logs "/tmp/packaging.log";
+        upload_logs "/tmp/storage.log";
+        upload_logs "/tmp/syslog";
+        upload_logs "/tmp/program.log";
+    }
+}
+
+1;
+
+# vim: set sw=4 et:
+
