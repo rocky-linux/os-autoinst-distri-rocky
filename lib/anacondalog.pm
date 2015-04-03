469d8d2e925a4fb0a6a96c9f6d730c71809330ea
Garret Raziel <boloomka@gmail.com>
2015-04-03 13:42:59 +0200

    upload Anaconda ABRT directory
    Differential Revision: https://phab.qadevel.cloud.fedoraproject.org/D310

--- lib/anacondalog.pm
+++ lib/anacondalog.pm
@@ -5,6 +5,13 @@ use testapi;
 
 sub post_fail_hook {
     my $self = shift;
+
+    my $has_traceback = 0;
+    if (check_screen "anaconda_error", 10) {
+        assert_and_click "anaconda_report_btn"; # Generage Anaconda ABRT logs
+        $has_traceback = 1;
+    }
+
     send_key "ctrl-alt-f2";
     my $logged_in = 0;
     if (get_var("LIVE") && check_screen "text_console_login", 20) {
@@ -29,9 +36,11 @@ sub post_fail_hook {
         upload_logs "/tmp/dnf.log";
 
         # Upload all ABRT logs
-        type_string "cd /var/tmp/abrt && tar czvf abrt.tar.gz *";
-        send_key "ret";
-        upload_logs "/var/tmp/abrt/abrt.tar.gz";
+        if ($has_traceback) {
+            type_string "cd /var/tmp && tar czvf var_tmp.tar.gz *";
+            send_key "ret";
+            upload_logs "/var/tmp/var_tmp.tar.gz";
+        }
 
         # Upload Anaconda logs
         type_string "tar czvf /tmp/anaconda_tb.tar.gz /tmp/anaconda-tb-*";
