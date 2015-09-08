f8f242b7e0415bd15ea7631fe0fa77441776c410
Jan Sedlák <jsedlak@redhat.com>
2015-09-08 15:54:22 +0200

    add guided shrink test
    Differential Revision: https://phab.qadevel.cloud.fedoraproject.org/D561

---
+++ tests/disk_guided_shrink.pm
@@ -0,0 +1,39 @@
+use base "anacondatest";
+use strict;
+use testapi;
+
+sub run {
+    my $self = shift;
+    # Anaconda hub
+    # Go to INSTALLATION DESTINATION and ensure one disk is selected.
+    $self->select_disks();
+    assert_and_click "anaconda_spoke_done";
+
+    # the only provided disk should be automatically selected and full
+    assert_and_click "anaconda_install_destination_reclaim_space_btn";
+
+    # select first partition
+    assert_and_click "anaconda_install_destination_reclaim_space_first_partition";
+
+    # shrink partition
+    assert_and_click "anaconda_install_destination_reclaim_space_shrink_btn";
+    assert_and_click "anaconda_install_destination_reclaim_space_shrink_slider";
+
+    assert_and_click "anaconda_install_destination_reclaim_space_btn";
+
+    # Anaconda hub
+    assert_screen "anaconda_main_hub", 300; #
+
+}
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return { fatal => 1 };
+}
+
+1;
+
+# vim: set sw=4 et:
---
+++ tests/disk_guided_shrink_postinstall.pm
@@ -0,0 +1,26 @@
+use base "installedtest";
+use strict;
+use testapi;
+
+sub run {
+    assert_screen "root_console";
+    # mount first partition and check that it's intact
+    type_string 'reset; mount /dev/vda1 /mnt; echo $?';
+    send_key "ret";
+    assert_screen "console_command_success";
+    type_string 'reset; cat /mnt/testfile';
+    send_key "ret";
+    assert_screen "provided_disk_intact";
+}
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return { fatal => 1 };
+}
+
+1;
+
+# vim: set sw=4 et:
