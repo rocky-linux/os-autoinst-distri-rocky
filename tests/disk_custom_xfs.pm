7240ce774f0158951ad6ecc8594565bee893205a
Jan Sedlák <jsedlak@redhat.com>
2015-11-26 13:50:45 +0100

    add custom partitioning xfs tests
    Differential Revision: https://phab.qadevel.cloud.fedoraproject.org/D663

---
+++ tests/disk_custom_xfs.pm
@@ -0,0 +1,37 @@
+use base "anacondatest";
+use strict;
+use testapi;
+
+sub run {
+    my $self = shift;
+    # Go to INSTALLATION DESTINATION and ensure the disk is selected.
+    # Because PARTITIONING starts with 'custom_', this will select custom.
+    $self->select_disks();
+    assert_and_click "anaconda_spoke_done";
+
+    # Manual partitioning spoke should be displayed. Select Standard
+    # Partition scheme
+    $self->custom_scheme_select("standard");
+    # Do 'automatic' partition creation
+    assert_and_click "anaconda_part_automatic";
+    # Change root partition to xfs
+    $self->custom_change_fs("xfs");
+    assert_and_click "anaconda_spoke_done";
+    assert_and_click "anaconda_part_accept_changes";
+
+    # Anaconda hub
+    assert_screen "anaconda_main_hub", 300; #
+
+}
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return { fatal => 1 };
+}
+
+1;
+
+# vim: set sw=4 et:
