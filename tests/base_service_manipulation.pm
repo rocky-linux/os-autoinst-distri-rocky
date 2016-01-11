63e03ecbdf6198b19f04020c94614354323f8fb2
Adam Williamson <awilliam@redhat.com>
2016-01-11 12:30:24 -0800

    add base_service_manipulation test
    Summary:
    Not much to say, pretty much just implements the test case using
    some commands I dug up that give us handy 0/1 exit statuses.
    The assert_script_run function (from testapi) simply runs a
    command/script and passes or fails based on the exit status;
    we use a handy bash-ism when we *want* the exit status to be 1.
    
    Test Plan: Run the test and check that it passes (properly).
    
    Reviewers: jskladan, garretraziel
    
    Reviewed By: garretraziel
    
    Subscribers: tflink
    
    Differential Revision: https://phab.qadevel.cloud.fedoraproject.org/D713

---
+++ tests/base_service_manipulation.pm
@@ -0,0 +1,61 @@
+use base "installedtest";
+use strict;
+use testapi;
+
+sub run {
+    my $self=shift;
+    # wait for boot to complete
+    $self->boot_to_login_screen("", 30);
+    # switch to TTY3 for both, graphical and console tests
+    $self->root_console(tty=>3);
+    # we could make this slightly more 'efficient' by assuming sshd
+    # is always going to be enabled/running at first, but it's safer
+    # to force an expected starting state.
+    script_run "systemctl stop sshd.service";
+    script_run "systemctl disable sshd.service";
+    script_run "reboot";
+    $self->boot_to_login_screen("", 30);
+    $self->root_console(tty=>3);
+    # note the use of ! here is a bash-ism, but it sure makes life easier
+    assert_script_run '! systemctl is-enabled sshd.service';
+    assert_script_run '! systemctl is-active sshd.service';
+    assert_script_run '! ps -C sshd';
+    script_run "systemctl start sshd.service";
+    assert_script_run '! systemctl is-enabled sshd.service';
+    assert_script_run 'systemctl is-active sshd.service';
+    assert_script_run 'ps -C sshd';
+    script_run "systemctl stop sshd.service";
+    assert_script_run '! systemctl is-enabled sshd.service';
+    assert_script_run '! systemctl is-active sshd.service';
+    assert_script_run '! ps -C sshd';
+    script_run "systemctl enable sshd.service";
+    assert_script_run 'systemctl is-enabled sshd.service';
+    assert_script_run '! systemctl is-active sshd.service';
+    assert_script_run '! ps -C sshd';
+    script_run "reboot";
+    $self->boot_to_login_screen("", 30);
+    $self->root_console(tty=>3);
+    assert_script_run 'systemctl is-enabled sshd.service';
+    assert_script_run 'systemctl is-active sshd.service';
+    assert_script_run 'ps -C sshd';
+    script_run "systemctl disable sshd.service";
+    script_run "reboot";
+    $self->boot_to_login_screen("", 30);
+    $self->root_console(tty=>3);
+    assert_script_run '! systemctl is-enabled sshd.service';
+    assert_script_run '! systemctl is-active sshd.service';
+    assert_script_run '! ps -C sshd';
+}
+
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return { fatal => 1 };
+}
+
+1;
+
+# vim: set sw=4 et:
