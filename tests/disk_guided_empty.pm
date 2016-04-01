591b1532386fdd8baf5ca97d348abd9f05b612f5
Adam Williamson <awilliam@redhat.com>
2016-04-01 08:00:47 -0700

    revise updates.img test to work with new anaconda (T759)
    Summary:
    per details in T759, the 'unipony' updates image we use to test
    the updates image features doesn't work with latest anaconda (f24
    and Rawhide). I've built a new updates image which uses a neat
    anaconda feature that allows you to override CSS with a file in
    a special location; it sets the background for disk capacity
    texts on the INSTALLATION DESTINATION spoke to be pink. This
    lets us use a simple needle that just looks for a pink blob on
    that spoke, on the basis that it's unlikely there'll ever be a
    pink blob there for any other reason, so if there is one, the
    updates image worked. There will be an accompanying tools diff
    to change the updates disk image to use the new updates image.
    
    Test Plan:
    Do a test run and check the updates image tests pass
    and no other tests are broken. You'll need to pull in the tools
    diff and re-generate the updates disk image to check that test,
    the scsi_updates_img test should work with just this diff.
    
    Reviewers: jskladan, garretraziel
    
    Reviewed By: garretraziel
    
    Subscribers: tflink
    
    Differential Revision: https://phab.qadevel.cloud.fedoraproject.org/D799

--- tests/disk_guided_empty.pm
+++ tests/disk_guided_empty.pm
@@ -11,7 +11,7 @@ sub run {
     # updates.img tests work by changing the appearance of the INSTALLATION
     # DESTINATION screen, so check that if needed.
     if (get_var('BOOT_UPDATES_IMG_URL')){
-        assert_screen "anaconda_install_destination_pony", 30;
+        assert_screen "anaconda_install_destination_updates", 30;
     }
 
     assert_and_click "anaconda_spoke_done";
