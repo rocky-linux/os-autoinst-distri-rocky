d4466100d4d7e5cc3c45d1e344d10308432fb222
Jan Sedlák <jsedlak@redhat.com>
2016-05-04 09:58:35 +0200

    add necessary needles and fix code for KDE upgrade tests
    Differential Revision: https://phab.qadevel.cloud.fedoraproject.org/D837

--- tests/upgrade_postinstall_desktop.pm
+++ tests/upgrade_postinstall_desktop.pm
@@ -7,16 +7,18 @@ sub run {
     my $self = shift;
     my $password = get_var("USER_PASSWORD", "weakpassword");
 
-    # wait for GDM to appear
+    # wait for DM to appear
     $self->boot_to_login_screen("graphical_login", 20);
 
     # login as normal user
-    send_key "ret";
+    if (get_var("DESKTOP") eq 'gnome') {
+        send_key "ret";
+    }
     assert_screen "graphical_login_input";
     type_string $password;
     send_key "ret";
     # wait until desktop appears
-    assert_screen "graphical_desktop_clean", 30;
+    assert_screen "graphical_desktop_clean", 60;
     # check an upgrade actually happened (and we can log into a console)
     $self->root_console(tty=>3);
     $self->check_release(lc(get_var('VERSION')));
