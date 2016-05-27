802c9298fd5836b6d7fb6a0a11c979d6445fbe9d
Jan Sedlák <jsedlak@redhat.com>
2016-05-27 12:36:15 +0200

    add base_services_start test for ARM
    Differential Revision: https://phab.qadevel.cloud.fedoraproject.org/D861

--- tests/base_services_start.pm
+++ tests/base_services_start.pm
@@ -4,8 +4,13 @@ use testapi;
 
 sub run {
     my $self=shift;
-    # wait for boot to complete
-    $self->boot_to_login_screen("", 30);
+    if (get_var("ARCH") eq "arm") {
+        # we cannot use boot_to_login_screen, wait_still_screen is unusable during ARM boot (it shows black screen for first few minutes)
+        assert_screen "text_console_login", 150;
+    } else {
+        # wait for boot to complete
+        $self->boot_to_login_screen("", 30);
+    }
     # switch to TTY3 for both, graphical and console tests
     $self->root_console(tty=>3);
     validate_script_output 'systemctl --failed', sub { $_ =~ m/0 loaded units/ };
