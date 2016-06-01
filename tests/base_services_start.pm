05f33bbd3998b2ce3c4040c806bb9851f8420be3
Jan Sedlák <jsedlak@redhat.com>
2016-06-01 08:12:16 +0200

    workaround mcelog failing
    Differential Revision: https://phab.qadevel.cloud.fedoraproject.org/D843

--- tests/base_services_start.pm
+++ tests/base_services_start.pm
@@ -13,7 +13,12 @@ sub run {
     }
     # switch to TTY3 for both, graphical and console tests
     $self->root_console(tty=>3);
-    validate_script_output 'systemctl --failed', sub { $_ =~ m/0 loaded units/ };
+    my $output = script_output 'systemctl --failed';
+    if ($output =~ /1 loaded units/ && $output =~ /mcelog.service/) {
+        record_soft_failure;
+    } elsif (! $output =~ /0 loaded units/) {
+        die "Services other than mcelog failed to load";
+    }
 }
 
 
