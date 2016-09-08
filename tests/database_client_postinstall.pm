1c5fffa859f95364ab21cb8373fbed8ed1c3df89
Adam Williamson <awilliam@redhat.com>
2016-09-08 14:19:09 -0700

    add a database server role test (and client test)
    Summary:
    Pretty straightforward tests which deploy the database
    server role and exercise it a bit.
    
    Test Plan: Run the tests, check they work properly.
    
    Reviewers: jskladan, garretraziel
    
    Reviewed By: garretraziel
    
    Subscribers: tflink
    
    Differential Revision: https://phab.qadevel.cloud.fedoraproject.org/D991

---
+++ tests/database_client_postinstall.pm
@@ -0,0 +1,37 @@
+use base "installedtest";
+use strict;
+use testapi;
+use lockapi;
+
+sub run {
+    my $self=shift;
+    # clone host's /etc/hosts (for phx2 internal routing to work)
+    # must come *before* setup_tap_static or else it would overwrite
+    # its changes
+    $self->clone_host_file("/etc/hosts");
+    # set up networking
+    $self->setup_tap_static("10.0.2.105", "dbclient.domain.local");
+    # clone host's resolv.conf to get name resolution
+    $self->clone_host_file("/etc/resolv.conf");
+    # use compose repo, disable u-t, etc.
+    $self->repo_setup();
+    # install postgresql
+    assert_script_run "dnf -y install postgresql", 120;
+    # wait for the server to be ready
+    mutex_lock "db_ready";
+    mutex_unlock "db_ready";
+    # check we can connect to the database and create a table
+    assert_script_run "PGPASSWORD=correcthorse psql openqa -h 10.0.2.104 -U openqa -c 'CREATE TABLE test2 (testcol int);'";
+}
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return { fatal => 1 };
+}
+
+1;
+
+# vim: set sw=4 et:
---
+++ tests/role_deploy_database_server_postinstall.pm
@@ -0,0 +1,55 @@
+use base "installedtest";
+use strict;
+use testapi;
+use lockapi;
+use mmapi;
+
+sub run {
+    my $self=shift;
+    $self->clone_host_file("/etc/hosts");
+    # set up networking
+    $self->setup_tap_static("10.0.2.104", "db.domain.local");
+    # clone host's resolv.conf to get name resolution
+    $self->clone_host_file("/etc/resolv.conf");
+    # use compose repo, disable u-t, etc.
+    $self->repo_setup();
+    # deploy the database server role
+    assert_script_run 'echo \'{"database":"openqa","owner":"openqa","password":"correcthorse"}\' | rolectl deploy databaseserver --settings-stdin', 300;
+    # check the role status, should be 'running'
+    validate_script_output 'rolectl status databaseserver/1', sub { $_ =~ m/^running/ };
+    # check 'settings' output looks vaguely right
+    validate_script_output 'rolectl settings databaseserver/1', sub {$_ =~ m/owner = openqa/ };
+    # check we can connect to the database and create a table
+    assert_script_run 'su postgres -c "psql openqa -c \'CREATE TABLE test (testcol int);\'"';
+    # check we can add a row to the table
+    assert_script_run 'su postgres -c "psql openqa -c \'INSERT INTO test VALUES (5);\'"';
+    # check we can query the table
+    validate_script_output 'su postgres -c "psql openqa -c \'SELECT * FROM test;\'"', sub {$_ =~ m/^ *testcol.*5.*1 row/s };
+    # check we can modify the row
+    assert_script_run 'su postgres -c "psql openqa -c \'UPDATE test SET testcol = 50 WHERE testcol = 5;\'"';
+    validate_script_output 'su postgres -c "psql openqa -c \'SELECT * FROM test;\'"', sub {$_ =~ m/^ *testcol.*50.*1 row/s };
+    # we're all ready for other jobs to run!
+    mutex_create('db_ready');
+    wait_for_children;
+    # once child jobs are done, stop the role
+    assert_script_run 'rolectl stop databaseserver/1';
+    # check role is stopped
+    validate_script_output 'rolectl status databaseserver/1', sub { $_ =~ m/^ready-to-start/ };
+    # decommission the role
+    assert_script_run 'rolectl decommission databaseserver/1', 120;
+    # check role is decommissioned
+    validate_script_output 'rolectl list instances', sub { $_ eq "" };
+}
+
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return { fatal => 1 };
+}
+
+1;
+
+# vim: set sw=4 et:
