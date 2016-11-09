65cadb11df4877d2a8e454a6cdd843a1b0961254
Adam Williamson <awilliam@redhat.com>
2016-11-09 07:20:17 -0800

    Collect some data on freshly-installed systems after some tests
    Summary:
    I've been wanting to do this for a while; I think it'll let us
    check for some significant changes between composes. This should
    cause runs of a few test cases to collect and upload info on:
    
    * installed packages
    * free memory
    * disk space
    * active services
    * 1 minute of CPU usage info (via top)
    
    immediately after install and initial login. In some cases this
    will be useful / interesting simply to look at directly, but
    we can also have check-compose analyze the data and include
    significant changes in its reports.
    
    Test Plan:
    Run affected tests, make sure the data collection
    works.
    
    Reviewers: jskladan, garretraziel
    
    Reviewed By: garretraziel
    
    Subscribers: tflink
    
    Differential Revision: https://phab.qadevel.cloud.fedoraproject.org/D1046

---
+++ tests/_collect_data.pm
@@ -0,0 +1,30 @@
+use base "installedtest";
+use strict;
+use testapi;
+
+sub run {
+    my $self = shift;
+    $self->root_console(tty=>3);
+    assert_script_run 'rpm -qa --queryformat "%{NAME}\n" | sort -u > /var/tmp/rpms.log';
+    upload_logs '/var/tmp/rpms.log';
+    assert_script_run 'free > /var/tmp/free.log';
+    upload_logs '/var/tmp/free.log';
+    assert_script_run 'df > /var/tmp/df.log';
+    upload_logs '/var/tmp/df.log';
+    assert_script_run 'systemctl -t service --no-pager |grep -o ".*\.service" > /var/tmp/services.log';
+    upload_logs '/var/tmp/services.log';
+    assert_script_run 'top -i -n20 -b > /var/tmp/top.log';
+    upload_logs '/var/tmp/top.log';
+}
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return {};
+}
+
+1;
+
+# vim: set sw=4 et:
