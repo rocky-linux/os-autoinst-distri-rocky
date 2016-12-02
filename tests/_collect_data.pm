6f90ff01439d0bec1f7b66d5556db70f0b3b072a
Adam Williamson <awilliam@redhat.com>
2016-12-02 15:01:40 -0800

    collect_data: run top *before* everything else
    Committing without review as this is pretty trivial. Running
    top *after* all the other collect_data tasks leads to some
    meaningless fluctuations in the output; I intended to look for
    activity caused by stuff running 'as usual', not activity from
    the other collect_data tasks.

--- tests/_collect_data.pm
+++ tests/_collect_data.pm
@@ -5,6 +5,8 @@ use testapi;
 sub run {
     my $self = shift;
     $self->root_console(tty=>3);
+    assert_script_run 'top -i -n20 -b > /var/tmp/top.log';
+    upload_logs '/var/tmp/top.log';
     assert_script_run 'rpm -qa --queryformat "%{NAME}\n" | sort -u > /var/tmp/rpms.log';
     upload_logs '/var/tmp/rpms.log';
     assert_script_run 'free > /var/tmp/free.log';
@@ -13,8 +15,6 @@ sub run {
     upload_logs '/var/tmp/df.log';
     assert_script_run 'systemctl -t service --no-pager |grep -o ".*\.service" > /var/tmp/services.log';
     upload_logs '/var/tmp/services.log';
-    assert_script_run 'top -i -n20 -b > /var/tmp/top.log';
-    upload_logs '/var/tmp/top.log';
 }
 
 sub test_flags {
