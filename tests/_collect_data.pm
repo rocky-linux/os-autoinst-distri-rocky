e9fda18016c932ec7e7643d6feb5f4b7b32060e7
Adam Williamson <awilliam@redhat.com>
2017-03-02 14:47:44 -0800

    Explicit timeout for the 'top' in _collect_data
    This keeps failing because the default `assert_script_run`
    timeout changed from 90 to 30 in the last os-autoinst update
    (an unintended consequence of a change I made). This has been
    fixed upstream, but in the mean time, let's just set an
    explicit timeout on the call.

--- tests/_collect_data.pm
+++ tests/_collect_data.pm
@@ -5,7 +5,7 @@ use testapi;
 sub run {
     my $self = shift;
     $self->root_console(tty=>3);
-    assert_script_run 'top -i -n20 -b > /var/tmp/top.log';
+    assert_script_run 'top -i -n20 -b > /var/tmp/top.log', 120;
     upload_logs '/var/tmp/top.log';
     assert_script_run 'rpm -qa --queryformat "%{NAME}\n" | sort -u > /var/tmp/rpms.log';
     upload_logs '/var/tmp/rpms.log';
