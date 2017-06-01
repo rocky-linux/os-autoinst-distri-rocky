48b99a2291c1a5c97b07ed74bd4f390b2208b8f9
Jan Sedlák <jsedlak@redhat.com>
2017-06-01 11:06:04 +0200

    add base system logging test
    Differential Revision: https://phab.qa.fedoraproject.org/D1202

---
+++ tests/base_system_logging.pm
@@ -0,0 +1,24 @@
+use base "installedtest";
+use strict;
+use testapi;
+
+sub run {
+    my $self=shift;
+    # switch to TTY3 for both, graphical and console tests
+    $self->root_console(tty=>3);
+    # Check that journalctl DOESN'T output "No entries". This is also the case when journal files are missing.
+    # NOTE: We are quietly assuming that something was logged in journal in last 30 minutes. Should be boot log,
+    # switch to TTY3 etc.
+    assert_script_run '! journalctl -aeb --since "30 minutes ago" | grep "\-\- No entries \-\-" -q';
+    # if rsyslog package is installed (e. g. Server edition), /var/log/secure should exist and be nonempty
+    assert_script_run '(! rpm --quiet -q rsyslog) || [ -s /var/log/secure ]';
+}
+
+
+sub test_flags {
+    return { fatal => 1 };
+}
+
+1;
+
+# vim: set sw=4 et:
