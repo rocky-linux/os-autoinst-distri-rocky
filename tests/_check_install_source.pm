d119e1e53e64500e5af9e5dac7ef270d5482ab96
Adam Williamson <awilliam@redhat.com>
2017-07-10 10:21:57 -0700

    Check for NFS repo message in anaconda.log too
    It seems to have moved there in F27.

--- tests/_check_install_source.pm
+++ tests/_check_install_source.pm
@@ -19,7 +19,8 @@ sub run {
     if ($repourl =~ s/^nfs://) {
         # the above both checks if we're dealing with an NFS URL, and
         # strips the 'nfs:' from it if so
-        assert_script_run "grep 'mounting ${repourl}' /tmp/packaging.log";
+        # message is in packaging.log up to F26, anaconda.log F27+
+        assert_script_run "grep 'mounting ${repourl}' /tmp/packaging.log /tmp/anaconda.log";
     }
     else {
         assert_script_run "grep \"added repo: 'anaconda'.*${repourl}\" /tmp/packaging.log";
