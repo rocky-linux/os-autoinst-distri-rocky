e6fe996613b27b59105de03ed39a97b0a68876fa
Michel Normand <normand@linux.vnet.ibm.com>
2017-08-28 03:55:04 -0400

    Revert "Modify "install_multi" test for PowerPC"
    This reverts commit 8b2977f1d618316ded61420df4fc7d2afd07cbf4.
    
    The initial commit was required for PowerPC
    until qemu 2.7.1-6 (in f25) not required anymore
    since qemu 2.9.0-5 (in f26)

--- tests/disk_guided_multi_postinstall.pm
+++ tests/disk_guided_multi_postinstall.pm
@@ -5,13 +5,7 @@ use testapi;
 sub run {
     assert_screen "root_console";
     # check that second disk is intact
-    if (get_var('OFW')) {
-        # on PowerPC, installation disk is second disk (vdb)
-        # so need to check vda
-        assert_script_run 'mount /dev/vda1 /mnt';
-    } else {
-        assert_script_run 'mount /dev/vdb1 /mnt';
-    }
+    assert_script_run 'mount /dev/vdb1 /mnt';
     validate_script_output 'cat /mnt/testfile', sub { $_ =~ m/Hello, world!/ };
 }
 
