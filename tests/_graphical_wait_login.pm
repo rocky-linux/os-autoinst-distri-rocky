e2a8c34e8678c0ceaa9f1410f27f235b0bb43806
Adam Williamson <awilliam@redhat.com>
2018-03-16 15:10:34 -0700

    Tweak mouse placement in _graphical_wait_login for KDE browser
    This mouse placement is in the middle of where the 'install
    addon' popover appears in Firefox, and that seems like it
    sometimes causes the popover to immediately disappear in KDE.
    This is pretty corner-case-y so I don't wanna report it as a
    bug, let's just tweak the cursor hiding location and see if it
    solves the problem.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/_graphical_wait_login.pm
+++ tests/_graphical_wait_login.pm
@@ -91,7 +91,7 @@ sub run {
         }
 
         # Move the mouse somewhere it won't highlight the match areas
-        mouse_set(300, 200);
+        mouse_set(300, 800);
         # KDE can take ages to start up
         check_desktop_clean(tries=>40);
     }
