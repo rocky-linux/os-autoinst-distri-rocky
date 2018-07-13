7621cd7e5715cef8316616ecb855a36f19f2942a
Adam Williamson <awilliam@redhat.com>
2018-07-13 14:02:31 -0700

    Move the new base_services_start check to the start
    ...otherwise we actually return before it, if no services fail.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/base_services_start.pm
+++ tests/base_services_start.pm
@@ -6,6 +6,11 @@ sub run {
     my $self = shift;
     # switch to TTY3 for both, graphical and console tests
     $self->root_console(tty=>3);
+    # "Job foo.service/start deleted to break ordering cycle"-type
+    # message in the log indicates a service got taken out of the boot
+    # process to resolve some kind of dependency loop, see e.g.
+    # https://bugzilla.redhat.com/show_bug.cgi?id=1600823
+    assert_script_run "! journalctl -b | grep 'deleted to break ordering'";
     # dump the systemctl output
     assert_script_run "systemctl --failed | tee /tmp/failed.txt";
     # if we have 0 failed services, we're good
@@ -21,11 +26,6 @@ sub run {
         assert_script_run "systemctl is-failed mcelog.service";
         record_soft_failure;
     }
-    # "Job foo.service/start deleted to break ordering cycle"-type
-    # message in the log indicates a service got taken out of the boot
-    # process to resolve some kind of dependency loop, see e.g.
-    # https://bugzilla.redhat.com/show_bug.cgi?id=1600823
-    assert_script_run "! journalctl -b | grep 'deleted to break ordering'";
 }
 
 
