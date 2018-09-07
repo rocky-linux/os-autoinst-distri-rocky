e2eb794a87d166ad3180616353e5e86a306827af
Adam Williamson <awilliam@redhat.com>
2018-09-06 20:36:29 -0700

    Tweak the workaround yet again (with soft fail this time)
    Sigh, sorry, just perfecting. This way it won't fail when the
    bug is fixed (hopefully).
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/_graphical_wait_login.pm
+++ tests/_graphical_wait_login.pm
@@ -70,22 +70,22 @@ sub run {
         }
     }
     if (get_var("DESKTOP") eq 'gnome' && get_var("INSTALL_NO_USER")) {
+        # wait for the stupid 'help' screen to show and kill it. Also,
         # FIXME 2018-09: #1625572 means g-i-s 'first login' mode runs
         # after 'user creation' mode on F29 and Rawhide install tests,
-        # even though it shouldn't. Drop this conditional and just do
-        # getting_started once 1625572 is fixed
-        if ($version > 28 || $version eq "Rawhide") {
-            gnome_initial_setup();
-        }
-        else {
-            # wait for the stupid 'help' screen to show and kill it
-            if (check_screen "getting_started", 30) {
+        # even though it shouldn't. So, let's check for that too.
+        if (check_screen ["next_button", "getting_started"], 120) {
+            if (match_has_tag "getting_started") {
                 send_key "alt-f4";
                 wait_still_screen 5;
             }
             else {
-                record_soft_failure "'getting started' missing (probably BGO#790811)";
+                record_soft_failure "gnome-initial-setup ran in both modes, probably RHBZ#1625572";
+                gnome_initial_setup();
             }
+        }
+        else {
+            record_soft_failure "'getting started' missing (probably BGO#790811)";
         }
     }
 
