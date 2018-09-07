7dff1843db173f3d82547c7ef93d6348502b2c01
Adam Williamson <awilliam@redhat.com>
2018-09-06 19:53:07 -0700

    Tweak that workaround again (forgot the version check)
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/_graphical_wait_login.pm
+++ tests/_graphical_wait_login.pm
@@ -72,18 +72,21 @@ sub run {
     if (get_var("DESKTOP") eq 'gnome' && get_var("INSTALL_NO_USER")) {
         # FIXME 2018-09: #1625572 means g-i-s 'first login' mode runs
         # after 'user creation' mode on F29 and Rawhide install tests,
-        # even though it shouldn't. Go back to the commented-out lines
-        # when this is fixed
-        gnome_initial_setup();
-
-        # wait for the stupid 'help' screen to show and kill it
-        #if (check_screen "getting_started", 30) {
-        #    send_key "alt-f4";
-        #    wait_still_screen 5;
-        #}
-        #else {
-        #    record_soft_failure "'getting started' missing (probably BGO#790811)";
-        #}
+        # even though it shouldn't. Drop this conditional and just do
+        # getting_started once 1625572 is fixed
+        if ($version > 28 || $version eq "Rawhide") {
+            gnome_initial_setup();
+        }
+        else {
+            # wait for the stupid 'help' screen to show and kill it
+            if (check_screen "getting_started", 30) {
+                send_key "alt-f4";
+                wait_still_screen 5;
+            }
+            else {
+                record_soft_failure "'getting started' missing (probably BGO#790811)";
+            }
+        }
     }
 
     # Move the mouse somewhere it won't highlight the match areas
