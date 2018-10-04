0bf76db7d59de934f32f7fec12796019fcca881f
Adam Williamson <awilliam@redhat.com>
2018-10-04 15:19:43 -0700

    Add a test of bootchain stuff for updates
    This adds a new test intended to just check boot chain things
    for updates. It doesn't run any test modules besides the stock
    update ones, but sets a variable, ADVISORY_BOOT_TEST, which
    causes _advisory_update to do some additional stuff after
    installing the updates but before rebooting: it forces regen
    of the initramfs and bootloader config, and reinstalls the
    bootloader on BIOS (not UEFI as it's not relevant). If the
    following boot fails, we probably have a bug somewhere.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/_advisory_update.pm
+++ tests/_advisory_update.pm
@@ -9,6 +9,16 @@ sub run {
     # update packages and run 'dnf update'
     $self->root_console(tty=>3);
     repo_setup;
+    if (get_var("ADVISORY_BOOT_TEST")) {
+        # to test boot stuff - in case the update touched grub2, or dracut,
+        # or anything adjacent - let's force-regenerate the initramfs and
+        # the bootloader config, and reinstall the bootloader on BIOS. This
+        # is kinda arch-dependent, but works for the three arches currently
+        # in openQA: x86_64, ppc64le, and aarch64.
+        assert_script_run "dracut -f";
+        assert_script_run 'grub2-mkconfig -o $(readlink -m /etc/grub2.cfg)';
+        assert_script_run "grub2-install /dev/vda" unless (get_var("UEFI"));
+    }
     # reboot, in case any of the updates need a reboot to apply
     script_run "reboot", 0;
 }
