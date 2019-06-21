9e3b16bfcfddc0065e70aa9cbdebb5913e80bb63
Lukas Ruzicka <lruzicka@redhat.com>
2019-06-21 14:54:18 +0200

    Change Settings test due to change functionality.

--- tests/apps_startstop/gnome/settings.pm
+++ tests/apps_startstop/gnome/settings.pm
@@ -3,13 +3,8 @@ use strict;
 use testapi;
 use utils;
 
-# This test tests that Settings starts and the it can be used to
-# change the desktop settings. 
-# This test was originally used to set the virtual machine desktop
-# to black to support other tests. This functionality has been taken
-# by the Terminal test.
-# You can put this test anywhere in the suite without any problems.
-
+# This test tests that Settings starts
+#
 sub run {
     my $self = shift;
     # start the settings application
@@ -20,26 +15,7 @@ sub run {
     # select Background menu item
     assert_and_click 'apps_settings_menu_background';
     wait_still_screen 5;
-    assert_and_click 'apps_settings_choose_background';
-    wait_still_screen 5;
     
-    # select the Background color menu and move down until black is found, then click it.
-    assert_and_click 'apps_settings_choose_color';
-    wait_still_screen 5;
-    my $black_visible = 0;
-    while ($black_visible == 0) {
-        send_key 'down';
-        if (check_screen('apps_settings_black_visible', 1)) {
-            $black_visible = 1;
-        }
-    }
-    assert_and_click 'apps_settings_black_visible';
-    wait_still_screen 5;
-
-    # confirm the selection
-    assert_and_click 'apps_settings_black_select';
-    wait_still_screen 5;
-
     # close the application
     send_key 'alt-f4';
     wait_still_screen 5;
