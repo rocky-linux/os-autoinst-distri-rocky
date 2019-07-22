5a10c537f7564ff10e5c1939afe3e2233e611b29
Adam Williamson <awilliam@redhat.com>
2019-07-22 14:54:36 -0700

    Update potential_conflict invocation for Python 3
    The script got ported. Yay.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/mediakit_fileconflicts.pm
+++ tests/mediakit_fileconflicts.pm
@@ -10,11 +10,8 @@ sub run {
     assert_script_run "mount /dev/cdrom /mnt/iso";
     # download the check script
     assert_script_run "curl -o /usr/local/bin/potential_conflict.py https://pagure.io/fedora-qa/qa-misc/raw/master/f/potential_conflict.py";
-    # install python2 and yum - script needs these for now:
-    # https://pagure.io/fedora-qa/qa-misc/issue/4
-    script_run "dnf -y install yum python2", 180;
     # run the check
-    assert_script_run "python2 /usr/local/bin/potential_conflict.py --repofrompath=media,/mnt/iso -r media";
+    assert_script_run "/usr/local/bin/potential_conflict.py --repofrompath=media,/mnt/iso -r media";
 }
 
 sub test_flags {
