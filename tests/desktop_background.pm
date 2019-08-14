469a1395d0b34aae6bc1dc9bb6201285cac705e8
Adam Williamson <awilliam@redhat.com>
2019-08-14 14:30:25 -0700

    Revise desktop background checks
    Split this out of install_default, because it really is not a
    part of that test and we do not want that test to fail because
    the desktop background is wrong. Make it its own test module
    and test suite instead. Don't do it on Rawhide, because we
    really can't assert anything worthwhile about Rawhide at the
    moment at least (this means the test runs but is a no-op and
    will always pass on Rawhide, unfortunately). Move the needles
    to a more appropriate location (this has nothing to do with
    anaconda) and use 'background' not 'wallpaper' naming (that's
    the name we use elsewhere in the project, e.g. package names).
    Also, run the test on updates, and add an F29 needle for this
    purpose.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

---
+++ tests/desktop_background.pm
@@ -0,0 +1,17 @@
+use base "installedtest";
+use strict;
+use testapi;
+
+sub run {
+    check_desktop_clean;
+    # If we want to check that there is a correct background used, as a part
+    # of self identification test, we will do it here. For now we don't do
+    # this for Rawhide as Rawhide doesn't have its own backgrounds and we
+    # don't have any requirement for what background Rawhide uses.
+    my $version = get_var('VERSION');
+    assert_screen "${version}_background" if ($version ne "Rawhide");
+}
+
+sub test_flags {
+    return { fatal => 1 };
+}
