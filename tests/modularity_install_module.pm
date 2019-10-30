907e27cd8fdbc3ee04e609ac0e1eb64cd9c52975
Lukáš Růžička <lruzicka@redhat.com>
2019-10-30 14:58:26 +0100

    Change nodejs module version in the install test to make it pass.

--- tests/modularity_install_module.pm
+++ tests/modularity_install_module.pm
@@ -11,10 +11,10 @@ sub run {
     download_modularity_tests();
 
     # Check that modularity works and that a particular module is available in the system.
-    assert_script_run('/root/test.py -m nodejs -s 11 -a list');
+    assert_script_run('/root/test.py -m nodejs -s 12 -a list');
 
     # Check that module can be enabled and removed.
-    assert_script_run('/root/test.py -m nodejs -s 11 -p default -a install,remove,reset -f hard');
+    assert_script_run('/root/test.py -m nodejs -s 12 -p default -a install,remove,reset -f hard');
     
     # Upload modular logs
     upload_logs '/root/modular.log', failok=>1;
