4c05f19a9fa7824bd0bda4b8cb94aa4cefb12e50
Adam Williamson <awilliam@redhat.com>
2019-11-06 16:43:20 -0800

    Add QA:Testcase_Anaconda_User_Interface_VNC_Vncconnect tests
    This adds tests for vncconnect, a variation on VNC install using
    reverse VNC (where the client waits for the server to connect to
    it). Very similar to the regular-VNC test, but we have to use
    tigervnc as Boxes doesn't do reverse VNC.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

---
+++ tests/_vncconnect_client_setup.pm
@@ -0,0 +1,29 @@
+use base "installedtest";
+use strict;
+use lockapi;
+use tapnet;
+use testapi;
+use utils;
+
+sub run {
+    my $self = shift;
+    $self->root_console(tty=>3);
+    setup_tap_static('10.0.2.117', 'vnc004.domain.local');
+    # install tigervnc (Boxes doesn't do reverse VNC)
+    assert_script_run "dnf -y install tigervnc", 180;
+    # take down the firewall
+    assert_script_run "systemctl stop firewalld";
+    desktop_vt;
+    menu_launch_type 'terminal';
+    wait_still_screen 5;
+    type_safely "vncviewer -FullScreen -listen\n";
+    mutex_create 'vncconnect_client_ready';
+}
+
+sub test_flags {
+    return { fatal => 1 };
+}
+
+1;
+
+# vim: set sw=4 et:
