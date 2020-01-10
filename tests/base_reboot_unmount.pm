23bae35d823e9f3e42e9dbf72df9ddefd89923de
Lukáš Růžička <lruzicka@redhat.com>
2020-01-10 15:39:02 +0100

    Create a test to test unmount errors during reboot (aka Reboot Testcase)

---
+++ tests/base_reboot_unmount.pm
@@ -0,0 +1,42 @@
+use base "installedtest";
+use strict;
+use testapi;
+use utils;
+
+
+sub test_routine {
+    my $self = shift;
+    # Save the result of the error tracking grep operation on journalctl output.
+    script_run 'journalctl -b | grep -E "dirty bit|data may be corrupt|recovery|unmounted|recovering" > errors.txt';
+    # Count the number of errors.
+    my $errors_count = script_run "cat errors.txt | wc -l";
+    # Die, if errors have been found.
+    if ($errors_count != 0) {
+        die "Unmount errors have been found in journalctl.";
+    }
+
+}
+
+sub run {
+    # Seems that the first login is handled by the _console_wait_login(), so
+    # we can start with testing right away.
+
+    # Run test for the first time
+    test_routine();
+    # Reboot the system.
+    type_safely "reboot\n";
+    # This time, we will need to login manually.
+    boot_to_login_screen;
+    console_login;
+
+    # Run the tests for the second time.
+    test_routine();
+}
+
+sub test_flags {
+    return { fatal => 1 };
+}
+
+1;
+
+# vim: set sw=4 et:
