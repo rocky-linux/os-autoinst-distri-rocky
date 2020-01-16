1098450fd6ee0cad0c0c522e283f1fee900b3f09
Adam Williamson <awilliam@redhat.com>
2020-01-16 18:51:40 +0000

    Pull in F30 Cockpit 210 update as a workaround
    Otherwise the new cockpit main screen needle match fails, because
    'machine' is not on the top part of the screen.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -513,8 +513,9 @@ sub _repo_setup_updates {
     # a hash of releases and update IDs. if no workarounds are needed
     # for any release, the hash can be empty and this will do nothing
     my %workarounds = (
-        "30" => [],
-        "31" => []
+        # cockpit 210 so we get the fixed main screen
+        "30" => ["FEDORA-2020-8ff69018de"],
+        "31" => ["]
     );
     # then we'll download each update for our release:
     my $advisories = $workarounds{$version};
