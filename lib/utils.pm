66e53feb4ded6b339877b15973c4984d2e4a07d6
Adam Williamson <awilliam@redhat.com>
2020-02-05 02:51:37 -0800

    Update workarounds (drop update now stable, add F31 libdnf fix)
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -513,9 +513,9 @@ sub _repo_setup_updates {
     # a hash of releases and update IDs. if no workarounds are needed
     # for any release, the hash can be empty and this will do nothing
     my %workarounds = (
-        # cockpit 210 so we get the fixed main screen
-        "30" => ["FEDORA-2020-8ff69018de"],
-        "31" => []
+        "30" => [],
+        # libdnf fix for #1795004
+        "31" => ["FEDORA-2020-980b80f9d9"]
     );
     # then we'll download each update for our release:
     my $advisories = $workarounds{$version};
