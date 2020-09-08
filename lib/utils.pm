0350977ba3fb9450fc1cc741a4cfea0568939daf
Adam Williamson <awilliam@redhat.com>
2020-09-08 14:13:29 -0700

    Update workarounds: drop stable one, add F33 FreeIPA upgrade one
    The F32 FreeIPA update that changed the web UI has gone stable
    now, so remove it. A pki-core update has just come out that fixes
    F32 -> F33 FreeIPA server upgrade test: add this as a workaround
    for F33 so that test stops failing on every update.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -441,8 +441,8 @@ sub setup_workaround_repo {
     my %workarounds = (
         "31" => [],
         # FreeIPA 4.8.9 to make the UI consistent across releases
-        "32" => ["FEDORA-2020-dc32a8de93"],
-        "33" => []
+        "32" => [],
+        "33" => ["FEDORA-2020-2bef864cab"]
     );
     # then we'll download each update for our release:
     my $advisories = $workarounds{$version};
