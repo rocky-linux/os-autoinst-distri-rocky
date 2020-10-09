9de75af60b426d4e9dd7e9e297b26bc7cacc8f46
Adam Williamson <awilliam@redhat.com>
2020-10-09 16:57:21 -0700

    Drop f33-backgrounds from workarounds (it went stable)
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -441,9 +441,8 @@ sub setup_workaround_repo {
     my %workarounds = (
         "31" => [],
         "32" => [],
-        # f33-backgrounds: https://bugzilla.redhat.com/show_bug.cgi?id=1885792
         # freeipa: https://bugzilla.redhat.com/show_bug.cgi?id=1886205
-        "33" => ["FEDORA-2020-27f80050a2", "FEDORA-2020-e9e815177e"]
+        "33" => ["FEDORA-2020-e9e815177e"]
     );
     # then we'll download each update for our release:
     my $advisories = $workarounds{$version};
