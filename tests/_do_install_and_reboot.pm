3850a088bcf28149e4df18d10959db9866241225
Adam Williamson <awilliam@redhat.com>
2020-11-18 15:36:12 -0800

    Add mouse click to install-wait loop on VNC clients
    I noticed a pattern lately of VNC tests failing on Rawhide when
    we have a debug kernel (but passing with a regular kernel). On
    closer investigation I think there's simply a screen blank
    happening if the install process takes more than five minutes,
    and that's more likely with a debug kernel. This extends the
    loop we use to move the mouse every so often while waiting for
    the install to complete (which is meant to defeat this sort of
    thing) to also click the mouse, when we're a VNC client test. In
    a quick check this seemed to help.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/_do_install_and_reboot.pm
+++ tests/_do_install_and_reboot.pm
@@ -121,6 +121,9 @@ sub run {
     while ($timeout > 0) {
         # move the mouse a bit
         mouse_set 100, 100;
+        # also click, if we're a VNC client, seems just moving mouse
+        # isn't enough to defeat blanking
+        mouse_click if (get_var("VNC_CLIENT"));
         mouse_hide;
         last if (check_screen "anaconda_install_done", $interval);
         $timeout -= $interval;
