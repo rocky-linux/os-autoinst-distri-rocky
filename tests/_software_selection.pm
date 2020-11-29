7bdaec0f8db57feae0589a44cf6e34aa2349dc80
Adam Williamson <awilliam@redhat.com>
2020-11-28 17:05:24 -0800

    Check both anaconda.log and packaging.log for software selection
    It seems the message got moved to anaconda.log in Rawhide. I
    think it should be fine to just check both.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/_software_selection.pm
+++ tests/_software_selection.pm
@@ -21,7 +21,7 @@ sub run {
         elsif (get_var('SUBVARIANT') eq 'Workstation') {
             $env = 'workstation-product-environment';
         }
-        assert_script_run "grep 'selected env' /tmp/packaging.log | tail -1 | grep $env";
+        assert_script_run "grep 'selected env' /tmp/anaconda.log /tmp/packaging.log | tail -1 | grep $env";
         send_key "ctrl-alt-f6";
         assert_screen "anaconda_main_hub", 30;
         return;
