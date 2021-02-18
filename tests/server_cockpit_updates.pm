9226ccabe33731aace74c088d52a9004f1ab9397
Adam Williamson <awilliam@redhat.com>
2021-02-18 14:30:50 -0800

    cockpit_updates: break out of loop if we see success
    Behaviour changed a bit in Cockpit 238, we may now just hit
    success during this loop, so handle that. Also use 'break' for
    the other two cases, not a big run counter bump.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/server_cockpit_updates.pm
+++ tests/server_cockpit_updates.pm
@@ -46,18 +46,19 @@ sub run {
         # and relog for further interaction. We will check if reconnection is
         # needed and if so, we will restart Firefox and login again.
         #
+        break if (check_screen("cockpit_updates_updated"));
         if (check_screen("cockpit_updates_reconnect", 1)) {
             quit_firefox;
             sleep 5;
             start_cockpit(1);
             select_cockpit_update();
-            $run = 50;
+            break;
 
         }
         # Ignore rebooting the system because we want to finish the test instead.
         elsif (check_screen('cockpit_updates_restart_ignore', 1)) {
             assert_and_click 'cockpit_updates_restart_ignore';
-            $run = 50;
+            break;
         }
         else {
             sleep 10;
