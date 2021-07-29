5b1d3517fef902cc3b4a482abd7d73b56692ba6e
Adam Williamson <awilliam@redhat.com>
2021-07-29 12:09:34 -0700

    Open Documents folder on Silverblue
    The open dialog on Silverblue (which is apparently not at all
    the same thing as the open dialog on Workstation, though they
    look the same) does not default to the Documents folder, so we
    have to open it.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/applications/evince/aaa_setup.pm
+++ tests/applications/evince/aaa_setup.pm
@@ -47,16 +47,21 @@ sub run {
     # Open the test file to create a starting point for the other Evince tests.
     # Click on Open button to open the File Open Dialog
     assert_and_click("evince_open_file_dialog", button => "left", timeout => 30);
-    
+
+    if (get_var("CANNED")) {
+        # open the Documents folder.
+        assert_and_click("evince_documents", button => "left", timeout => 30);
+    }
+
     # Select the evince.pdf file.
     assert_and_click("evince_file_select_pdf", button => "left", timeout => 30);
     
     # Click the Open button to open the file
     assert_and_click("gnome_button_open", button => "left", timeout => 30);
-    
+
     # Fullsize the Evince window.
     send_key("super-up");
-    
+
     # Check that the file has been successfully opened.
     assert_screen("evince_file_opened");
 }
