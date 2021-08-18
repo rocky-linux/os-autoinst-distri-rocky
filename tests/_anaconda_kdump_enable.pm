d5cd63a7cba3ab0d6f210acce161ffae5b10fee2
Trevor Cooper <tcooper@rockylinux.org>
2021-08-17 18:21:31 -0700

    wait for anaconda main hub before attempting kdump spoke

--- tests/_anaconda_kdump_enable.pm
+++ tests/_anaconda_kdump_enable.pm
@@ -6,6 +6,9 @@ use tapnet;
 
 sub run {
     my $self = shift;
+    # Anaconda hub
+    assert_screen "anaconda_main_hub", 300; #
+
     assert_and_click "anaconda_main_hub_kdump";
     assert_and_click "anaconda_kdump_enable";
     assert_screen "anaconda_kdump_enabled", 90;
