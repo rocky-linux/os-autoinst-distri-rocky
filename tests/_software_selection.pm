a01d76944a3225b05775f5cb182c4caa0e1e54d2
Al Bowles <akatch@users.noreply.github.com>
2021-08-20 23:34:30 -0500

    Stub out update to package group selection test
    Update the text of the log line to be checked for package group
    installation.
    
    Per issue #18, we will confirm the correct package group names and the
    criteria under which each should be selected.

--- tests/_software_selection.pm
+++ tests/_software_selection.pm
@@ -14,18 +14,16 @@ sub run {
         # we can't or don't want to check the selected package set in these cases
         return if (get_var('CANNED') || get_var('LIVE') || get_var('MEMCHECK'));
         $self->root_console;
-        my $env = 'custom-environment';
+        my $env = 'graphical-server-environment';
         if (get_var('SUBVARIANT') eq 'Server') {
             $env = 'server-product-environment';
         }
         elsif (get_var('SUBVARIANT') eq 'Workstation') {
             $env = 'workstation-product-environment';
         }
-        # pre-F35 line looks like:
-        # 07:51:39,382 INF modules.payloads.payload.dnf.utils: selected environment: custom-environment
-        # F35+ line looks like:
-        # 07:40:26,614 DBG ui.lib.software: Selecting the 'custom-environment' environment.
-        assert_script_run "egrep '(selected env|Selecting the.*environment)' /tmp/anaconda.log /tmp/packaging.log | tail -1 | grep $env";
+        # RL8 line looks like:
+        # 02:25:18,525 INF packaging: selected environment: graphical-server-environment
+        assert_script_run "egrep 'selected environment:' /tmp/anaconda.log /tmp/packaging.log | tail -1 | grep $env";
         send_key "ctrl-alt-f6";
         assert_screen "anaconda_main_hub", 30;
         return;
