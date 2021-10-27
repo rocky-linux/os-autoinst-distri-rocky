78f994722101f1f125bc0d89a4734846a618c3cc
Trevor Cooper <tcooper@rockylinux.org>
2021-10-27 09:01:56 -0700

    Rocky has kdump which changes text mode main hub install menu

--- tests/install_text.pm
+++ tests/install_text.pm
@@ -44,9 +44,10 @@ sub run {
         "source" => 3,
         "swselection" => 4,
         "destination" => 5,
-        "network" => 6,
-        "rootpwd" => 7,
-        "user" => 8
+        "kdump" => 6,
+        "network" => 7,
+        "rootpwd" => 8,
+        "user" => 9
     );
 
     # The error message that we are going to check for in the text installation
