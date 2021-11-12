0bf66050253415c13179d21d095c9d1aaa64253b
Lukas Magauer <42647570+lumarel@users.noreply.github.com>
2021-11-12 02:39:34 +0100

    Fixes multiple tests in regard of the PACKAGE_SETs `graphical-server` and `workstation` (#61)
    tcooper confirmed on the call. Merging it.
    
    * Extend product test commands
    
    * Increase disk size for `install_standard_partition_ext4` because some PACKAGE_SETs are too big
    
    * Limit the license acceptance to only run on tests which run for ISO installations
    It is unfortunately not that easy to limit this to ISOs due to that the misterous
    limit to HDD_1 and not `custom_resize_lvm`
    
    * Make sure that the disk post installs run in a shell
    it happens that the previous test ends in the GUI
    
    Co-authored-by: lumarel <lumarel@users.noreply.github.com>

--- tests/disk_custom_lvmthin_postinstall.pm
+++ tests/disk_custom_lvmthin_postinstall.pm
@@ -1,8 +1,13 @@
 use base "installedtest";
 use strict;
 use testapi;
+use utils;
 
 sub run {
+    my $self = shift;
+    unless (check_screen "root_console", 0) {
+        $self->root_console(tty=>4);
+    }
     assert_screen "root_console";
     # check that lvmthinpool is present:
     # http://atodorov.org/blog/2015/04/14/how-to-find-if-lvm-volume-is-thinly-provisioned/
