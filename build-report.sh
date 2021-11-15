6b67dd19d82fadf3575f0c2c54af1aeda4685353
Al Bowles <akatch@users.noreply.github.com>
2021-11-14 20:44:00 -0600

    Include BUILD_NAME in report header

--- build-report.sh
+++ build-report.sh
@@ -3,7 +3,7 @@ set -e
 
 BUILD_NAME=$1
 
-printf 'Test | Result\n=============\n'
+printf '# %s\nTest | Result\n=============\n' "$BUILD_NAME"
 openqa-cli api -X GET jobs build="$BUILD_NAME" | \
     jq -r '.jobs[] | {name,result} | join(" | ") | split("-") | last' | \
     sort
