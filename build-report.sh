dfb7232f28b0378051c4159adba2057b3ec73cac
Al Bowles <akatch@users.noreply.github.com>
2021-11-16 01:57:02 -0600

    Added @tcooper's suggestion

--- build-report.sh
+++ build-report.sh
@@ -3,10 +3,11 @@ set -e
 
 BUILD_NAME=$1
 
-printf '# %s\n' "$BUILD_NAME"
+printf '# Build%s\n' "$BUILD_NAME"
 printf "| Test | Result | Failure Reason | Effort to Fix | Notes |\n"
 printf "| ---- | ------ | -------------- | ------------ | ----- |\n"
 
 openqa-cli api -X GET jobs build="$BUILD_NAME" | \
     jq -r '.jobs[] | {name,result} | join(" | ") | split("-") | last' | \
-    sort
+    sort | \
+    sed 's,^,| ,g;s,$, | | | |,g'
