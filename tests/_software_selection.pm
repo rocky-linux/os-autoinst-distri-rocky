82a8191c97f29774c7e1fc423e5da6cc1ad02448
Al Bowles <akatch@users.noreply.github.com>
2022-02-15 22:07:55 -0600

    Correctly check for minimal-iso default packageset

--- tests/_software_selection.pm
+++ tests/_software_selection.pm
@@ -7,18 +7,14 @@ sub run {
     # Anaconda hub
     assert_screen "anaconda_main_hub", 300; #
 
-    # Select package set. Default is 'graphical-server', unless FLAVOR=minimal, where default is 'server'.
+    # Select package set.
     # If 'default' is specified, skip selection, but verify correct default
-    my $packageset = get_var('PACKAGE_SET', 'graphical-server');
+    my $packageset = get_var('PACKAGE_SET', 'default');
     if ($packageset eq 'default') {
-        # if PACKAGE_SET=default, actually check for these:
-        # dvd-iso = graphical-server
-        # minimal-iso = server
-        # boot-iso = graphical-server
         $self->root_console;
         my $env = "graphical-server-environment";
-        if ( get_var('FLAVOR') eq 'minimal-iso') {
-            $env = "server-environment";
+        if (get_var('FLAVOR') eq 'minimal-iso') {
+            $env = "server-product-environment";
         }
 
         assert_script_run "egrep 'selected environment:' /tmp/anaconda.log /tmp/packaging.log | tail -1 | grep $env";
@@ -38,18 +34,11 @@ sub run {
     # In Rocky, graphical-server starts out selected in the DVD ISO so if that's
     # what we're looking for we're done
     if (!check_screen("anaconda_".$packageset."_selected", 1)) {
-      # select desired environment
-      # go through the list 20 times at max (to prevent infinite loop when it's missing)
-      for (my $i = 0; !check_screen("anaconda_".$packageset."_highlighted", 1) && $i < 20; $i++) {
-        send_key "down";
-      }
-
+      send_key_until_needlematch("anaconda_".$packageset."_highlighted", "down", 20);
       send_key "spc";
     }
 
-    # check that desired environment is selected
     assert_screen "anaconda_".$packageset."_selected";
-
     assert_and_click "anaconda_spoke_done";
 
     # Anaconda hub
