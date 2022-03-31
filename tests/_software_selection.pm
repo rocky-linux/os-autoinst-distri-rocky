ff8ce569c7946c6ce2c7dbf58627516b2438e918
akatch <akatch@users.noreply.github.com>
2022-03-31 18:23:01 -0500

    Test Suite: install_default on rocky-8.5-[minimal|boot|dvd]-iso-x86_64 (#77)
    Merging per testing team conversation meeting.
    
    * Correctly check for default packagesets for each flavor
    
    * FIF updates for default packagesets
    
    - Set DESKTOP=gnome to trigger graphical_wait_login instead of
      console_wait_login
    - Increase virtual disk size to account for default packagesets
    
    * Correctly check for minimal-iso default packageset
    
    * Updated needles for issue 16
    
    * Update templates.fif.json with package set test suites
    
    Package sets tested per ISO will follow these conventions:
    - Test suites will not be duplicated between the boot and dvd ISOs
    - All default package sets will be tested against dvd-iso
    - Boot ISO will be considered valid if `minimal` package set installs
      cleanly
    
    * Do not add updated needle with changed hostname

--- tests/_software_selection.pm
+++ tests/_software_selection.pm
@@ -7,25 +7,17 @@ sub run {
     # Anaconda hub
     assert_screen "anaconda_main_hub", 300; #
 
-    # Select package set. Minimal is the default, if 'default' is specified, skip selection,
-    # but verify correct default in some cases
-    my $packageset = get_var('PACKAGE_SET', 'minimal');
-    if ($packageset eq 'default' || get_var('MODULAR')) {
-        # we can't or don't want to check the selected package set in these cases
-        return if (get_var('CANNED') || get_var('LIVE') || get_var('MEMCHECK') || (get_var('DISTRI') eq 'Rocky'));
+    # Select package set.
+    # If 'default' is specified, skip selection, but verify correct default
+    my $packageset = get_var('PACKAGE_SET', 'default');
+    if ($packageset eq 'default') {
         $self->root_console;
-        my $env = 'custom-environment';
-        if (get_var('SUBVARIANT') eq 'Server') {
-            $env = 'server-product-environment';
+        my $env = "graphical-server-environment";
+        if (get_var('FLAVOR') eq 'minimal-iso') {
+            $env = "server-product-environment";
         }
-        elsif (get_var('SUBVARIANT') eq 'Workstation') {
-            $env = 'workstation-product-environment';
-        }
-        # pre-F35 line looks like:
-        # 07:51:39,382 INF modules.payloads.payload.dnf.utils: selected environment: custom-environment
-        # F35+ line looks like:
-        # 07:40:26,614 DBG ui.lib.software: Selecting the 'custom-environment' environment.
-        assert_script_run "egrep '(selected env|Selecting the.*environment)' /tmp/anaconda.log /tmp/packaging.log | tail -1 | grep $env";
+
+        assert_script_run "egrep 'selected environment:' /tmp/anaconda.log /tmp/packaging.log | tail -1 | grep $env";
         send_key "ctrl-alt-f6";
         assert_screen "anaconda_main_hub", 30;
         return;
@@ -42,18 +34,12 @@ sub run {
     # In Rocky, graphical-server starts out selected in the DVD ISO so if that's
     # what we're looking for we're done
     if (!check_screen("anaconda_".$packageset."_selected", 1)) {
-      # select desired environment
-      # go through the list 20 times at max (to prevent infinite loop when it's missing)
-      for (my $i = 0; !check_screen("anaconda_".$packageset."_highlighted", 1) && $i < 20; $i++) {
-        send_key "down";
-      }
-
+      send_key_until_needlematch("anaconda_".$packageset."_highlighted", "down", 20);
       send_key "spc";
     }
 
     # check that desired environment is selected
     assert_screen "anaconda_".$packageset."_selected";
-
     assert_and_click "anaconda_spoke_done";
 
     # Anaconda hub
