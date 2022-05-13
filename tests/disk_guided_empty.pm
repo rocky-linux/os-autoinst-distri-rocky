9a6c1b73df5688a8de23fb1eb5f6340a967e3ac2
Al Bowles <akatch@users.noreply.github.com>
2022-05-12 22:42:11 -0500

    Fix indentations

--- tests/disk_guided_empty.pm
+++ tests/disk_guided_empty.pm
@@ -18,16 +18,16 @@ sub run {
     if (get_var('TEST_UPDATES')){
         assert_screen "anaconda_install_destination_updates", 30;
     }
-   # Here the self identification test code is placed.
-   my $branched = get_var('VERSION');
-   if ($identification eq 'true' or $branched ne "Rawhide") {
+    # Here the self identification test code is placed.
+    my $branched = get_var('VERSION');
+    if ($identification eq 'true' or $branched ne "Rawhide") {
         check_top_bar(); # See utils.pm
         # we don't check version or pre-release because here those
         # texts appear on the banner which makes the needling
         # complex and fragile (banner is different between variants,
         # and has a gradient so for RTL languages the background color
         # differs; pre-release text is also translated)
-   }
+    }
 
     assert_and_click "anaconda_spoke_done";
 
