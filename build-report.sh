9ec23d61158b0267b53dd7bcf92f2bcef44d1abc
Al Bowles <akatch@users.noreply.github.com>
2022-05-15 21:53:08 -0500

    Fix this header

--- build-report.sh
+++ build-report.sh
@@ -5,7 +5,7 @@ BUILD_NAME=$1
 
 printf '# Build%s\n' "$BUILD_NAME"
 printf "| Test | Result | Failure Reason | Effort to Fix | Notes |\n"
-printf "| ---- | ------ | -------------- | ------------ | ----- |\n"
+printf "| ---- | ------ | -------------- | ------------- | ----- |\n"
 
 openqa-cli api -X GET jobs build="$BUILD_NAME" | \
     jq -r '.jobs[] | {name,result} | join(" | ") | split("-") | last' | \
