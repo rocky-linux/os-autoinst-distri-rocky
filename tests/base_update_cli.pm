8106d07b6cfa67a8a2d9976e01f54702bb3399c1
Trevor Cooper <tcooper@rockylinux.org>
2023-04-29 10:41:51 -0700

    correct order for enable powertools

--- tests/base_update_cli.pm
+++ tests/base_update_cli.pm
@@ -11,13 +11,13 @@ sub run {
     prepare_test_packages;
     # check rpm agrees they installed good
     verify_installed_packages;
-    # update the fake pandoc-common (should come from the real repo)
-    # this can take a long time if we get unlucky with the metadata refresh
-    assert_script_run 'dnf -y --disablerepo=openqa-testrepo* --disablerepo=updates-testing update pandoc-common', 600;
     if (get_var("DISTRI") eq "rocky") {
         # pandoc-common is in PowerTools in RockyLinux
         assert_script_run 'dnf config-manager --set-enabled powertools', 60;
     }
+    # update the fake pandoc-common (should come from the real repo)
+    # this can take a long time if we get unlucky with the metadata refresh
+    assert_script_run 'dnf -y --disablerepo=openqa-testrepo* --disablerepo=updates-testing update pandoc-common', 600;
     # check we got the updated version
     verify_updated_packages;
     # now remove pandoc-common, and see if we can do a straight
