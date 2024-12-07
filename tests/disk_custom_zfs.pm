d31b6dd44923739e996c1e705c51f03082db07e6
AlanMarshall <alangm1001@gmail.com>
2024-12-07 15:44:20 +0000

    Initial changes for dummy run

---
+++ tests/disk_custom_zfs.pm
@@ -0,0 +1,34 @@
+use base "anacondatest";
+use strict;
+use testapi;
+use anaconda;
+
+sub run {
+    my $self = shift;
+    # Go to INSTALLATION DESTINATION and ensure the disk is selected.
+    # Because PARTITIONING starts with 'custom_', this will select custom.
+    select_disks();
+    assert_and_click "anaconda_spoke_done";
+
+    # Manual partitioning spoke should be displayed. Select Standard
+    # Partition scheme
+    custom_scheme_select("standard");
+    # Do 'automatic' partition creation
+    assert_and_click "anaconda_part_automatic";
+    # Change root partition to xfs
+    custom_change_fs("xfs");
+    assert_and_click "anaconda_spoke_done";
+    assert_and_click "anaconda_part_accept_changes";
+
+    # Anaconda hub
+    assert_screen "anaconda_main_hub", 300;    #
+
+}
+
+sub test_flags {
+    return {fatal => 1};
+}
+
+1;
+
+# vim: set sw=4 et:
