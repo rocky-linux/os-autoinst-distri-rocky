e9fcb82397774e5ee9f31163e25f474809c66257
AlanMarshall <alangm1001@gmail.com>
2024-12-09 14:35:32 +0000

    Add zfs install details.

--- tests/disk_custom_zfs_postinstall.pm
+++ tests/disk_custom_zfs_postinstall.pm
@@ -6,6 +6,12 @@ sub run {
     assert_screen "root_console";
     # check that xfs is used on root partition
     assert_script_run "mount | grep 'on / type xfs'";
+    script_run "dnf config-manager --set-enabled crb",300;
+    script_run "dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm",300;
+    script_run "dnf install -y https://zfsonlinux.org/epel/zfs-release-2-3.el9.noarch.rpm",300;
+    script_run "dnf install -y zfs",300;
+    script_run "modprobe zfs";
+    script_run "zpool list";
 }
 
 sub test_flags {
