3a2a47824bdef5f1e7f5d7359068e47b4bed7347
AlanMarshall <alangm1001@gmail.com>
2024-12-14 15:37:26 +0000

    Store HDD_1 for follow on tests and tweaks to get it working

--- tests/disk_custom_zfs_postinstall.pm
+++ tests/disk_custom_zfs_postinstall.pm
@@ -1,17 +1,29 @@
 use base "installedtest";
 use strict;
 use testapi;
+use utils;
 
 sub run {
     assert_screen "root_console";
     # check that xfs is used on root partition
     assert_script_run "mount | grep 'on / type xfs'";
     script_run "dnf config-manager --set-enabled crb",300;
+    script_run "dnf update --refresh -y",300;
+    type_safely "reboot\n";
+    boot_to_login_screen;
+    # This time, we will need to login manually.
+    console_login(user => "root", password => get_var("ROOT_PASSWORD"));
+    sleep 2;
     script_run "dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm",300;
+    # OpenZFS repo config
     script_run "dnf install -y https://zfsonlinux.org/epel/zfs-release-2-3.el9.noarch.rpm",300;
-    script_run "dnf install -y zfs",300;
+    # zfs cli utilities
+    script_run "dnf install -y --enablerepo=zfs-testing zfs",600;
     script_run "modprobe zfs";
     script_run "zpool list";
+    # DEBUG
+    script_run "rpm -qi zfs-release-2-3.el9.noarch";
+    script_run "rpm -qi zfs-2.2.7-1.el9.x86_64";
 }
 
 sub test_flags {
