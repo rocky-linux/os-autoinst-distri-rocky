cb3a2a5c8aa39e1808f879e50e4d4850b729e708
AlanMarshall <alangm1001@gmail.com>
2024-12-15 12:05:54 +0000

    Tweaks again

--- tests/disk_custom_zfs_postinstall.pm
+++ tests/disk_custom_zfs_postinstall.pm
@@ -11,19 +11,20 @@ sub run {
     script_run "dnf update --refresh -y",300;
     type_safely "reboot\n";
     boot_to_login_screen;
-    # This time, we will need to login manually.
+    # This time, need to login manually.
     console_login(user => "root", password => get_var("ROOT_PASSWORD"));
     sleep 2;
     script_run "dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm",300;
     # OpenZFS repo config
     script_run "dnf install -y https://zfsonlinux.org/epel/zfs-release-2-3.el9.noarch.rpm",300;
     # zfs cli utilities
-    script_run "dnf install -y --enablerepo=zfs-testing zfs",600;
+    # For test version, use --enablerepo=zfs-testing
+    script_run "dnf install -y zfs",720;
+    script_run 'mokutil --sb-state';
     script_run "modprobe zfs";
     script_run "zpool list";
     # DEBUG
-    script_run "rpm -qi zfs-release-2-3.el9.noarch";
-    script_run "rpm -qi zfs-2.2.7-1.el9.x86_64";
+    script_run "rpm -qi zfs-2.1.16-1.el9.x86_64";
 }
 
 sub test_flags {
