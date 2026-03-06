5c2350dd6dddb156b1ed03986b6b18c3f2d5605f
Trevor Cooper <tcooper@rockylinux.org>
2026-03-06 11:01:27 -0800

    Rocky 10+ graphical-server-environment needs firefox installed for cockpit

--- tests/_setup_browser.pm
+++ tests/_setup_browser.pm
@@ -10,20 +10,26 @@ sub run {
 
     # set up appropriate repositories
     repo_setup();
-    # use --enablerepo=fedora for Modular compose testing (we need to
-    # create and use a non-Modular repo to get some packages which
-    # aren't in Modular Server composes)
+
     my $extraparams = '';
-    $extraparams = '--enablerepo=fedora' if (get_var("MODULAR"));
-    # install a desktop and firefox so we can actually try it
-    assert_script_run "dnf ${extraparams} -y groupinstall 'base-x'", 300;
-    # FIXME: this should probably be in base-x...X seems to fail without
-    assert_script_run "dnf ${extraparams} -y install libglvnd-egl", 160;
-    # try to avoid random weird font selection happening
-    assert_script_run "dnf ${extraparams} -y install dejavu-sans-fonts dejavu-sans-mono-fonts dejavu-serif-fonts", 160;
-    # since firefox-85.0-2, firefox doesn't seem to run without this
-    assert_script_run "dnf ${extraparams} -y install dbus-glib", 160;
-    assert_script_run "dnf ${extraparams} -y install firefox", 160;
+
+    if ((get_var("DISTRI") eq "rocky") && (get_var("DESKTOP") eq "gnome") && (get_version_major() >= 10)) {
+        # base-x is removed from Rocky 10+ and install_default_upload produces
+        # a system with graphical-server-environment booted into GUI. Simply
+        # ensure firefox is installed.
+        assert_script_run "dnf ${extraparams} -y install firefox", 160;
+    }
+    else {
+        # install a desktop and firefox so we can actually try it
+        assert_script_run "dnf ${extraparams} -y groupinstall 'base-x'", 300;
+        # FIXME: this should probably be in base-x...X seems to fail without
+        assert_script_run "dnf ${extraparams} -y install libglvnd-egl", 160;
+        # try to avoid random weird font selection happening
+        assert_script_run "dnf ${extraparams} -y install dejavu-sans-fonts dejavu-sans-mono-fonts dejavu-serif-fonts", 160;
+        # since firefox-85.0-2, firefox doesn't seem to run without this
+        assert_script_run "dnf ${extraparams} -y install dbus-glib", 160;
+        assert_script_run "dnf ${extraparams} -y install firefox", 160;
+    }
 }
 
 sub test_flags {
