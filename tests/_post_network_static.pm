c1a3341653bfa2fdb262b78ce499779b4bcd59df
Trevor Cooper <tcooper@rockylinux.org>
2026-03-23 18:46:02 -0700

    console required for _post_network_static

--- tests/_post_network_static.pm
+++ tests/_post_network_static.pm
@@ -6,8 +6,20 @@ use utils;
 
 sub run {
     my $self = shift;
+    my $distri = get_var("DISTRI");
+    my $desktop = get_var("DESKTOP");
+    my $user_login = get_var("USER_LOGIN");
+
     my ($ip, $hostname) = split(/ /, get_var("POST_STATIC"));
     $hostname //= 'localhost.localdomain';
+
+    # switch to TTY3 for graphical tests where user login is not blocked
+    # NOTE: The choice of TTY3 comes from _console_wait_login test.
+    if ($distri eq "rocky" && $desktop eq "gnome" && $user_login ne "false") {
+        $self->root_console(tty => 3);
+        assert_screen("root_console");
+    }
+
     # set up networking
     setup_tap_static($ip, $hostname);
 }
